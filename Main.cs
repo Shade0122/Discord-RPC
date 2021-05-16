2293f31d2a6775f9a06e863594ca09ddb3122b22
Shade <73481203+Shade0122@users.noreply.github.com>
2021-05-16 14:16:56 +0100

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,13 +1,8 @@
-public Welcome()
-{
-  InitializeComponent();
-  
-  this.handlers = default(Classes.DiscordRPC.EventHandlers);
-  Classes.DiscordRPC.Initialize("Client ID", ref this.handlers, true, null);
-  this.presence.details = "Text 1";
-  this.presence.state = "Text 2";
-  this.presence.largeImageKey = "Large Image Name";
-  this.presence.smallImageKey = "Small Image Name";
-  this.presence.largeImageText = "Large Image Text";
-  Classes.DiscordRPC.UpdatePresence(ref this.presence);
-}
+this.handlers = default(Classes.DiscordRPC.EventHandlers);
+Classes.DiscordRPC.Initialize("Client ID", ref this.handlers, true, null);
+this.presence.details = "Text 1";
+this.presence.state = "Text 2";
+this.presence.largeImageKey = "Large Image Name";
+this.presence.smallImageKey = "Small Image Name";
+this.presence.largeImageText = "Large Image Text";
+Classes.DiscordRPC.UpdatePresence(ref this.presence);
