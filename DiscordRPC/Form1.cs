34ca2ad8debae1775a552e3cde3bab8a6ca72dbb
Shade <73481203+Shade0122@users.noreply.github.com>
2021-06-12 20:20:40 +0100

    Add files via upload

---
+++ DiscordRPC/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+
+namespace DiscordRPC
+{
+    public partial class Form1 : Form
+    {
+        private RPC.EventHandlers handlers;
+        private RPC.RichPresence presence;
+        public Form1()
+        {
+            InitializeComponent();
+
+            RPC.Initialize("Client ID", ref handlers, true, null);
+            presence.details = "RPC Details"; 
+            presence.state = "RPC State"; 
+            presence.largeImageKey = "Large Image Key"; 
+            presence.smallImageKey = "Small Image Key";
+            presence.largeImageText = "Large Image Text"; 
+            presence.smallImageText = "Small Image Text";
+            RPC.UpdatePresence(ref presence);
+        }
+    }
+}   
